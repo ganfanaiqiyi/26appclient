d1e7477aa6620a281bf45ddf0a9f390410467f6c
sb <sb@gmail.com>
2026-03-09 04:31:43 +0800

    init

---
+++ App.vue
@@ -0,0 +1,39 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			// #ifdef H5
+			uni.getSystemInfo({
+				success(e) {
+					/* 窗口宽度大于420px且不在PC页面且不在移动设备时跳转至 PC.html 页面 */
+					if (
+						e.windowWidth > 420 &&
+						!window.top.isPC &&
+						!/iOS|Android/i.test(e.system)
+					) {
+						// 跳转到PC端专用页面
+						window.location.pathname = "/static/html/pc.html";
+					}
+				},
+			});
+			// #endif
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+
+	/* #ifdef H5 */
+	page {
+		background-color: #141414;
+	}
+
+	/* #endif */
+</style>
---
+++ pages/remen/remen.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>
+		<!-- 引入播放器 -->
+		<!-- localhost:8080/h5是你自己的项目站点，如果放在生产环境，也需要自行替换生产环境的站点 -->
+		<!-- 引入播放器 -->
+		<!-- <yingbing-video src="https://d33h6qzhyg0joc.cloudfront.net/video/2025-05-23/7/1925690550269947904/085f1a11fd3140e5ba7eedf34cdaf0ae.m3u8?t=69a5d29a&us=2028525887184785411&sign=28b8d013ea3dd1d5cb6de4ca0ade868ea5633336"></yingbing-video> -->
+	</view>
+</template>
+
